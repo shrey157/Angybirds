edecbad03e3ef31c1bb7c95f74d3ee4f5318448d
shrey157 <shreyansh.gem@gmail.com>
2021-05-24 07:24:06 -0700

    platform colour (red)

--- sketch.js
+++ sketch.js
@@ -15,8 +15,9 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-
+   
     ground = new Ground(600,height,1200,20);
+   
     platform = new Ground(150, 305, 300, 170);
 
     box1 = new Box(700,320,70,70);
@@ -60,5 +61,6 @@ function draw(){
     log5.display();
 
     bird.display();
+    fill ('green')
     platform.display();
 }
